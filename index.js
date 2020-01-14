4c1d7d296ab109b51290b203c508a55847aeb05d
Yotsaphon <jew2121@gmail.com>
2020-01-14 15:45:22 +0700

    +1

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 //   console.log(n +' '+ typeof(n));
 
 let nn = document.querySelector('#n').textContent = n;
-let txt_output = "";
+let n = Number(new Promise(function(resolve, reject) {
+
+});)
 
   function fizzbuzz(n) {
     let output = [];
+    let txt_output = "";
+
     for (var i = 0; i < n; i++) {
       if (i%3 == 0 && i%5 == 0) {
         output.push("FizzBuzz ");
@@ -19,8 +23,13 @@ let txt_output = "";
         txt_output += "Buzz" ;
       }else {
         output.push(i);
+        txt_output += ''
       }
     }
+
+    document.querySelector('#txt_output').textContent = n;
+    return output;
   }
 
+document.querySelector('#nn').textContent = n;
 fizzbuzz(5);
