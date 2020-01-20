d0456973dc205b8aea10f798ae762b6e249bdbe8
Yotsaphon <jew2121@gmail.com>
2020-01-20 21:47:33 +0700

    s

--- cal.js
+++ cal.js
@@ -9,17 +9,17 @@ document.getElementById('inputbutton').onclick = function(){
 
   if (count<11) {
     if (guess > 100 || guess < 1) {
-      document.querySelector('result').textContent = "Please Enter Number Between 0 and 100";
+      document.querySelector('.result').textContent = "Please Enter Number Between 0 and 100";
     } else {
-      document.querySelector('counts').textContent = 10 - count + " left";
+      document.querySelector('.counts').textContent = (10 - count) + " left";
       if (guess == randomnumber) {
-        document.querySelector('result').textContent = " Congratulations! You got it right!";
+        document.querySelector('.result').textContent = " Congratulations! You got it right!";
         count = 12;
       }else if (guess > randomnumber) {
-        document.querySelector('result').textContent = " Your number was too high!";
+        document.querySelector('.result').textContent = " Your number was too high!";
         count++;
       }else {
-        document.querySelector('result').textContent = " Your number was too low!";
+        document.querySelector('.result').textContent = " Your number was too low!";
         count++;
       }
 
@@ -28,9 +28,9 @@ document.getElementById('inputbutton').onclick = function(){
       }else{
         previous += ", " + guess;
       }
-      document.querySelector('previous').textContent = previous;
+      document.querySelector('.previous').textContent = previous;
     }
     }else{
-    document.querySelector('result').textContent = " Better try next time the number is" + randomnumber +".";
+    document.querySelector('.result').textContent = " Better try next time the number is" + randomnumber +".";
   }
 }
