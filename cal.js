5462585df232dacfddec0dd6f4365d711b7e4f1c
Yotsaphon <jew2121@gmail.com>
2020-01-20 22:19:34 +0700

    d

--- cal.js
+++ cal.js
@@ -11,26 +11,44 @@ document.getElementById('inputbutton').onclick = function(){
     if (guess > 100 || guess < 1) {
       document.querySelector('.result').textContent = "Please Enter Number Between 0 and 100";
     } else {
-      document.querySelector('.counts').textContent = (10 - count) + " left";
+
       if (guess == randomnumber) {
-        document.querySelector('.result').textContent = " Congratulations! You got it right!";
+
+        document.querySelector('.resultright').textContent = " Congratulations! You got it right!";
         count = 12;
-      }else if (guess > randomnumber) {
-        document.querySelector('.result').textContent = " Your number was too high!";
+
+      }
+      
+      if (guess > randomnumber) {
+
+        document.querySelector('.resultwrong').textContent = " Your number was too high!";
         count++;
-      }else {
-        document.querySelector('.result').textContent = " Your number was too low!";
+
+      }else{
+
+        document.querySelector('.resultwrong').textContent = " Your number was too low!";
         count++;
+
       }
 
-      if (count == 2) {
+      if (count == 2 ) {
+        document.querySelector('.counts').textContent = (11 - count) + " left";
         previous = guess;
+        document.querySelector('.previous').textContent = previous;
+
+      }else if (count == 12) {
+        
       }else{
+        document.querySelector('.counts').textContent = (11 - count) + " left";
         previous += ", " + guess;
+        document.querySelector('.previous').textContent = previous;
+
       }
-      document.querySelector('.previous').textContent = previous;
+
+
     }
-    }else{
-    document.querySelector('.result').textContent = " Better try next time the number is" + randomnumber +".";
-  }
+    }if (count == 11) {
+      document.querySelector('.resultwrong').textContent = " Better try next time the number is " + randomnumber +".";
+      
+    }
 }
