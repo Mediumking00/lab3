4861ec12f7d026ce0ba5481f5cc8783c4f09c7a6
Yotsaphon <jew2121@gmail.com>
2020-01-20 20:52:21 +0700

    a

--- cal.js
+++ cal.js
@@ -1,7 +1,3 @@
-// alert('This is signal of html');
-//let n = Number(promtpt('Enter n for FizzBuzz Program'));
-//let nn = document.querySelector('#n').textContent = n;
-
 var randomnumber = Math.floor(Math.randon()*100)+1;
 
 var count = 0;
@@ -12,27 +8,30 @@ document.getElementById("input").onclick = function(){
   var guess = document.getElementById("inputbutton").value;
 
   if (count<=10) {
+    if (x > 100 || x < 1) {
+      document.querySelector('.result').textContent = "Please Enter Number Between 0 and 100";
+    } else {
 
-    document.querySelector('.counts').textContent = 10 - guess + " left";
+      document.querySelector('.counts').textContent = 10 - guess + " left";
 
-    if (guess == randomnumber) {
-      document.querySelector('.result').textContent = " Congratulations! You got it right!";
-    }else if (guess > randomnumber) {
-      count++;
-      document.querySelector('.result').textContent = " Your number was too high!";
-    }else {
-      count++;
-      document.querySelector('.result').textContent = " Your number was too low!";
+      if (guess == randomnumber) {
+        document.querySelector('.result').textContent = " Congratulations! You got it right!";
+      }else if (guess > randomnumber) {
+        count++;
+        document.querySelector('.result').textContent = " Your number was too high!";
+      }else {
+        count++;
+        document.querySelector('.result').textContent = " Your number was too low!";
+      }
+
+      if (count == 1) {
+        previous = guess;
+      }else{
+        previous += ", " + guess;
+      }
+      document.querySelector('.previous').textContent = previous;
     }
-
-    if (count == 1) {
-      previous = guess;
     }else{
-      previous += ", " + guess;
-    }
-    document.querySelector('.previous').textContent = previous;
-
-  }else{
     document.querySelector('.result').textContent = " Better try next time the number is" + randomnumber +".";
   }
 }
