dab45d5712a4a083c8ef3a7c6d6f26b55d62970d
Yotsaphon <jew2121@gmail.com>
2020-01-20 23:04:52 +0700

    ปผ

--- cal.js
+++ cal.js
@@ -1,6 +1,7 @@
 var randomnumber = Math.floor(Math.random()*100)+1;
 var count = 1;
 let previous;
+var check = 0;
 
 
 document.getElementById('inputbutton').onclick = function(){
@@ -8,43 +9,42 @@ document.getElementById('inputbutton').onclick = function(){
   var guess = document.getElementById('input').value;
 
   if (count<11) {
-    if (guess > 100 || guess < 1) {
-      document.querySelector('.result').textContent = "Please Enter Number Between 0 and 100";
-    } else {
-
-      if (guess == randomnumber) {
-
-        document.querySelector('.resultright').textContent = " Congratulations! You got it right!";
-        count = 12;
-
-      }
-      
+    if (guess <= 100 && guess >= 1) {
+      document.querySelector('.result').textContent = "";
       if (guess > randomnumber) {
-
+        
         document.querySelector('.resultwrong').textContent = " Your number was too high!";
         count++;
-
-      }else{
-
+        
+      }else if (guess < randomnumber ) {
+        
         document.querySelector('.resultwrong').textContent = " Your number was too low!";
         count++;
+        
+      }else {
+        document.querySelector('.resultwrong').textContent = "";
+        document.querySelector('.resultright').textContent = " Congratulations! You got it right!";
 
       }
+
+      
+     
 
       if (count == 2 ) {
         document.querySelector('.counts').textContent = (11 - count) + " left";
         previous = guess;
         document.querySelector('.previous').textContent = previous;
 
-      }else if (count == 12) {
+      }else if (count < 12 && count > 1) {
         
-      }else{
         document.querySelector('.counts').textContent = (11 - count) + " left";
         previous += ", " + guess;
         document.querySelector('.previous').textContent = previous;
 
       }
+    } else {
 
+      document.querySelector('.result').textContent = "Please Enter Number Between 0 and 100";
 
     }
     }if (count == 11) {
