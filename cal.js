18ceeefcd377b9ad9789997c920bd96a571ec291
Yotsaphon <jew2121@gmail.com>
2020-01-20 21:17:21 +0700

    w

--- cal.js
+++ cal.js
@@ -4,25 +4,28 @@ var count = 0;
 
 let previous;
 
-document.getElementsByClassName("form").getElementById("inputbutton").onclick = function(){
+document.querySelector('query')
 
-  var guess = document.getElementsByClassName("form").getElementById("input").value;
+
+document.getElementsByClassName('form').getElementById('inputbutton').onclick = function(){
+
+  var guess = document.getElementsByClassName('form').getElementById('input').value;
 
   if (count<=10) {
     if (guess > 100 || guess < 1) {
-      document.querySelector('.result').textContent = "Please Enter Number Between 0 and 100";
+      document.querySelector('result').textContent = "Please Enter Number Between 0 and 100";
     } else {
 
-      document.querySelector('.counts').textContent = 10 - guess + " left";
+      document.querySelector('counts').textContent = 10 - guess + " left";
 
       if (guess == randomnumber) {
-        document.querySelector('.result').textContent = " Congratulations! You got it right!";
+        document.querySelector('result').textContent = " Congratulations! You got it right!";
       }else if (guess > randomnumber) {
         count++;
-        document.querySelector('.result').textContent = " Your number was too high!";
+        document.querySelector('result').textContent = " Your number was too high!";
       }else {
         count++;
-        document.querySelector('.result').textContent = " Your number was too low!";
+        document.querySelector('result').textContent = " Your number was too low!";
       }
 
       if (count == 1) {
@@ -30,9 +33,9 @@ document.getElementsByClassName("form").getElementById("inputbutton").onclick = 
       }else{
         previous += ", " + guess;
       }
-      document.querySelector('.previous').textContent = previous;
+      document.querySelector('previous').textContent = previous;
     }
     }else{
-    document.querySelector('.result').textContent = " Better try next time the number is" + randomnumber +".";
+    document.querySelector('result').textContent = " Better try next time the number is" + randomnumber +".";
   }
 }
